831a7cab83d427f10326a4bdb0fd7747152fba43
raghusaripalli <rvs1604@gmail.com>
2021-02-27 12:13:52 -0500

    Longest Palindrome DP

--- LC/dp/LongestPalindromicSubstring.java
+++ LC/dp/LongestPalindromicSubstring.java
@@ -1,41 +1,50 @@
 package LC.dp;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestPalindromicSubstring {
-    boolean isPalindrome(String s) {
-        int N = s.length();
-        for (int i = 0; i < N / 2; i++) {
-            if (!(s.charAt(i) == s.charAt(N - i - 1)))
-                return false;
-        }
-        return true;
-    }
-
     public String longestPalindrome(String s) {
-        Set<String> mem = new HashSet<>();
-        String res = "";
         int N = s.length();
+        int start = 0, maxLength = 1;
+
+        if (N <= 1)
+            return s;
+
+        boolean[][] dp = new boolean[N][N];
+
+        // Each letter is a palindrome
         for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                String part = s.substring(i, j + 1);
-                if (!mem.contains(part)) {
-                    if (isPalindrome(part)) {
-                        mem.add(part);
-                        if (part.length() > res.length())
-                            res = part;
+            dp[i][i] = true;
+        }
+
+        // 2 letter base case
+        for (int i = 0; i < N - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        for (int k = 3; k <= N; k++) {
+            for (int i = 0; i < N - k + 1; i++) {
+                int j = i + k - 1;
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+
+                    if (k > maxLength) {
+                        start = i;
+                        maxLength = k;
                     }
                 }
             }
         }
-        return res;
+
+        return s.substring(start, start + maxLength);
     }
 
     public static void main(String[] args) {
         LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
-        longestPalindromicSubstring.longestPalindrome("a");
-        longestPalindromicSubstring.longestPalindrome("bb");
-        longestPalindromicSubstring.longestPalindrome("jkexvzsqshsxyytjmmhauoyrbxlgvdovlhzivkeixnoboqlfemfzytbolixqzwkfvnpacemgpotjtqokrqtnwjpjdiidduxdprngvitnzgyjgreyjmijmfbwsowbxtqkfeasjnujnrzlxmlcmmbdbgryknraasfgusapjcootlklirtilujjbatpazeihmhaprdxoucjkynqxbggruleopvdrukicpuleumbrgofpsmwopvhdbkkfncnvqamttwyvezqzswmwyhsontvioaakowannmgwjwpehcbtlzmntbmbkkxsrtzvfeggkzisxqkzmwjtbfjjxndmsjpdgimpznzojwfivgjdymtffmwtvzzkmeclquqnzngazmcfvbqfyudpyxlbvbcgyyweaakchxggflbgjplcftssmkssfinffnifsskmsstfclpjgblfggxhckaaewyygcbvblxypduyfqbvfcmzagnznquqlcemkzzvtwmfftmydjgvifwjoznzpmigdpjsmdnxjjfbtjwmzkqxsizkggefvztrsxkkbmbtnmzltbchepwjwgmnnawokaaoivtnoshywmwszqzevywttmaqvncnfkkbdhvpowmspfogrbmuelupcikurdvpoelurggbxqnykjcuoxdrpahmhiezaptabjjulitrilkltoocjpasugfsaarnkyrgbdbmmclmxlzrnjunjsaefkqtxbwoswbfmjimjyergjygzntivgnrpdxuddiidjpjwntqrkoqtjtopgmecapnvfkwzqxilobtyzfmeflqobonxiekvizhlvodvglxbryouahmmjtyyxshsqszvxekj");
+        System.out.println(longestPalindromicSubstring.longestPalindrome("a"));
+        System.out.println(longestPalindromicSubstring.longestPalindrome("bb"));
+        System.out.println(longestPalindromicSubstring.longestPalindrome("jkexvzsqshsxyytjmmhauoyrbxlgvdovlhzivkeixnoboqlfemfzytbolixqzwkfvnpacemgpotjtqokrqtnwjpjdiidduxdprngvitnzgyjgreyjmijmfbwsowbxtqkfeasjnujnrzlxmlcmmbdbgryknraasfgusapjcootlklirtilujjbatpazeihmhaprdxoucjkynqxbggruleopvdrukicpuleumbrgofpsmwopvhdbkkfncnvqamttwyvezqzswmwyhsontvioaakowannmgwjwpehcbtlzmntbmbkkxsrtzvfeggkzisxqkzmwjtbfjjxndmsjpdgimpznzojwfivgjdymtffmwtvzzkmeclquqnzngazmcfvbqfyudpyxlbvbcgyyweaakchxggflbgjplcftssmkssfinffnifsskmsstfclpjgblfggxhckaaewyygcbvblxypduyfqbvfcmzagnznquqlcemkzzvtwmfftmydjgvifwjoznzpmigdpjsmdnxjjfbtjwmzkqxsizkggefvztrsxkkbmbtnmzltbchepwjwgmnnawokaaoivtnoshywmwszqzevywttmaqvncnfkkbdhvpowmspfogrbmuelupcikurdvpoelurggbxqnykjcuoxdrpahmhiezaptabjjulitrilkltoocjpasugfsaarnkyrgbdbmmclmxlzrnjunjsaefkqtxbwoswbfmjimjyergjygzntivgnrpdxuddiidjpjwntqrkoqtjtopgmecapnvfkwzqxilobtyzfmeflqobonxiekvizhlvodvglxbryouahmmjtyyxshsqszvxekj"));
     }
 }
